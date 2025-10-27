e86ab78778c43ad0dd0bc5abdc288b51585059b2
Victor Henrique Souza <victor.souza1@alunos.sc.senac.br>
2025-10-27 13:40:03 -0300

    update 3

---
+++ index2.js
@@ -0,0 +1,10 @@
+var header = getElementByClassName('.menu a');
+var container = getElementByClassName('.container');
+
+function mudarTema() = {
+    header.style.backgroundColor = "#000";
+    container.style.BackgroundColor = "#000";
+    header.style.color = "#fff";
+    container.style.color="#fff";
+
+};
