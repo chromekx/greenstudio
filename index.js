40893e399383302f51f11bb7c014251b8a4c5ed7
Victor Henrique Souza <victor.souza1@alunos.sc.senac.br>
2025-11-02 23:49:27 -0300

    upgrade 11

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 let darkBtn = document.querySelector('.dark-btn');
 let items = document.querySelectorAll('.item');
-let containers = document.querySelectorAll('.containr');
+let containers = document.querySelectorAll('.container');
+let containersText = document.querySelectorAll('.container p');
 let menuBtn = document.querySelector('.icon');
-let menu = document.querySelector('.items');
 let carouselControlNext = document.querySelector('.carousel-control-next');
 let carouselControlPrev = document.querySelector('.carousel-control-prev');
+let menu = document.querySelector('.items');
 
 function darkTheme() {
     console.log('O botão de modo escuro foi pressionado.');
+    darkBtn.classList.toggle('dark-mode');
     items.forEach(item => item.classList.toggle('dark-mode'));
     containers.forEach(container => container.classList.toggle('dark-mode'));
-    darkBtn.classList.toggle('dark-mode');
+    containersText.forEach(textP => textP.classList.toggle('dark-mode'));
     menuBtn.classList.toggle('dark-mode');
     document.body.classList.toggle('dark-mode');
     carouselControlNext.classList.toggle('dark-mode');
